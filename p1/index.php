95705c21843cf1a72371ebb0f7ed412d3fce2689
Becca Lange <blange2176@gmail.com>
2020-09-14 20:47:04 -0400

    Added project 1 files

---
+++ p1/index.php
@@ -0,0 +1,8 @@
+<!doctype html> 
+<html lang='en'> 
+<head><title>Project 1 - Becca Lange</title>     
+	<meta charset='utf-8'> </head> 
+<body>     
+	<h1>Project 1 Application</h1> 
+</body> 
+</html>
