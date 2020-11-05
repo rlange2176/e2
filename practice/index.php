0f711f7f53e10eea5836679cfdbbbce0329986a6
Becca Lange <blange2176@gmail.com>
2020-11-05 17:43:56 -0500

    Add zipfoods project

--- practice/index.php
+++ practice/index.php
@@ -1,10 +1,25 @@
-<!doctype html>
-<html lang='en'>
-<head><title>Practice Application</title>     
-	<meta charset='utf-8'> 
-</head> 
-<body>     
-	<h1>Practice Application</h1> 
-	<p>Practice edit</p>
-</body> 
-</html>
+<?php
+
+function vowelCount($word)
+{
+    return substr_count($word, 'a')+substr_count($word, 'e')+substr_count($word, 'i')+substr_count($word, 'o')+substr_count($word, 'u');
+}
+
+$example1 = vowelCount('apple');
+$example2 = vowelCount('lynx');
+$example3 = vowelCount('hi');
+$example4 = vowelCount('mississippi');
+
+echo $example1;
+echo $example2;
+echo $example3;
+echo $example4;
+
+
+require 'index-view.php';
+require 'Person.php';
+
+$person = new Person('John', 'Harvard', 45);
+echo $person->firstName;
+echo $person->getFullName();
+echo $person->getClassification();
